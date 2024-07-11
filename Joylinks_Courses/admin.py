e95eb32c327a80534b1574e9e5408180e1655a27
Joylinks2024 <joylinkstelebot@gmail.com>
2024-07-12 01:59:13 +0500

    first commit

--- Joylinks_Courses/admin.py
+++ Joylinks_Courses/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from .models import Courses
 
+
 # Register your models here.
 
 @admin.register(Courses)
@@ -10,5 +11,6 @@ class JoyLinkUserAdmin(admin.ModelAdmin):
     list_editable = ['is_active']
     ordering = ['-is_active', 'title']
     search_fields = ['title']
-    # list_select_related = ['is_active', 'created_time', 'updated_time']
+    list_filter = ['id', 'title', 'is_active', 'created_time', 'updated_time']
     date_hierarchy = 'created_time'
+
--- users_app/admin.py
+++ users_app/admin.py
@@ -11,9 +11,10 @@ class JoyLinkUserAdmin(admin.ModelAdmin):
     list_display = ['full_name', "is_admin", 'call_state', 'off_state', 'is_active', 'is_ban', 'tg_id']
     list_editable = ['is_admin', 'is_ban', "is_active", 'call_state', 'off_state']
     search_fields = ['tg_id', 'is_admin', 'first_name', 'last_name']
-    list_filter = ['created_time', 'is_active', 'is_ban', 'is_admin', 'region', 'district','course_id', 'call_state', 'off_state']
+    list_filter = ['created_time', 'is_active', 'is_ban', 'is_admin', 'region', 'district', 'course_id', 'call_state',
+                   'off_state']
     filter_horizontal = ['course_id']
-    ordering = ['-is_active', 'first_name', '-created_time']
+    ordering = ['-is_active', '-is_ban', 'first_name', '-created_time']
     date_hierarchy = 'created_time'
     # list_select_related = ['is_active', 'created_time', 'updated_time', 'region', 'district']
     list_per_page = 100
